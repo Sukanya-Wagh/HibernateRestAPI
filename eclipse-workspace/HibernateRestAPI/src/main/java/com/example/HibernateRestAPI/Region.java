1416b4d48f711d87c5b13a102197104084181c2f
Sukanya-Wagh <waghsukanya153@gmail.com>
2025-07-20 14:34:24 +0530

    Update Region.java

--- eclipse-workspace/HibernateRestAPI/src/main/java/com/example/HibernateRestAPI/Region.java
+++ eclipse-workspace/HibernateRestAPI/src/main/java/com/example/HibernateRestAPI/Region.java
@@ -1,5 +1,33 @@
 package com.example.HibernateRestAPI;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "regions")
 public class Region {
 
+    @Id
+    private String regionId;
+
+    private String regionName;
+
+    // Getters and Setters
+
+    public String getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(String regionId) {
+        this.regionId = regionId;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
 }
