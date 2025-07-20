0224cae29850949bcf96830845b78a2925a69e69
Sukanya-Wagh <waghsukanya153@gmail.com>
2025-07-20 14:31:59 +0530

    Update CountryRepository.java

--- eclipse-workspace/HibernateRestAPI/src/main/java/com/example/HibernateRestAPI/CountryRepository.java
+++ eclipse-workspace/HibernateRestAPI/src/main/java/com/example/HibernateRestAPI/CountryRepository.java
@@ -1,43 +1,8 @@
 package com.example.HibernateRestAPI;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Entity
-@Table(name = "countries")
-public class Country {
-
-    @Id
-    private String countryId;
-
-    private String countryName;
-
-    private String regionId;
-
-    // Getters & Setters
-
-    public String getCountryId() {
-        return countryId;
-    }
-
-    public void setCountryId(String countryId) {
-        this.countryId = countryId;
-    }
-
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
-    }
-
-    public String getRegionId() {
-        return regionId;
-    }
-
-    public void setRegionId(String regionId) {
-        this.regionId = regionId;
-    }
+@Repository
+public interface CountryRepository extends JpaRepository<Country, String> {
 }
