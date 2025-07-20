8b36edc6144c7230104257bb4b0868a80e8fd806
Sukanya-Wagh <waghsukanya153@gmail.com>
2025-07-20 14:28:15 +0530

    Update CountryRepository.java

--- eclipse-workspace/HibernateRestAPI/src/main/java/com/example/HibernateRestAPI/CountryRepository.java
+++ eclipse-workspace/HibernateRestAPI/src/main/java/com/example/HibernateRestAPI/CountryRepository.java
@@ -1,5 +1,43 @@
 package com.example.HibernateRestAPI;
 
-public class CountryRepository {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    private String countryId;
+
+    private String countryName;
+
+    private String regionId;
+
+    // Getters & Setters
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(String regionId) {
+        this.regionId = regionId;
+    }
 }
